faebbfa4723d2345c23d5bf8e0163126fc96d4f8
Shwetalbhargav <hridaan.purav@gmail.com>
2023-11-08 22:39:27 +0530

    first commit

---
+++ src/university/conn.java
@@ -0,0 +1,19 @@
+package university;
+
+import java.sql.*;  
+
+public class conn{
+    Connection c;
+    Statement s;
+    public conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///ums","root","12345");    
+            s =c.createStatement();  
+            
+           
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
